3eada1cd26a3d27e119d23c05c468472dbdaa778
йцу <>
2024-12-04 16:46:19 +0300

    AutoCommit_4 декабря 2024 г. 16:46:19_SibNout2023

--- NirCmd/SpellFighter/SpellFighter.bat
+++ NirCmd/SpellFighter/SpellFighter.bat
@@ -10,24 +10,28 @@ title %~0
 set SpellName=HelloWorld.lnk
 set SpellRing=C:\ProgramData\Microsoft\Windows\Start Menu\Programs
 set SpellStone=C:\Windows\notepad.exe
+set SpellKey=ALT+CTRL+F
+set SpellKey=q
 (
 echo Set oWS = WScript.CreateObject("WScript.Shell"^)
-echo sLinkFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HelloWorld.lnk"
+echo sLinkFile = "%SpellRing%\%SpellName%"
 echo Set oLink = oWS.CreateShortcut(sLinkFile^)
-echo oLink.TargetPath = "C:\Windows\notepad.exe"
+echo oLink.TargetPath = "%SpellStone%"
 echo oLink.Arguments = ""
 echo oLink.Description = "MyProgram"
-::echo oLink.HotKey = "ALT+CTRL+F"
-::echo oLink.HotKey = "Q"
-echo oLink.HotKey = ""
-echo oLink.IconLocation = "C:\Program Files\MyApp\MyProgram.EXE, 2"
+::echo oLink.HotKey="ALT+CTRL+F"
+echo oLink.HotKey="%SpellKey%"
+::echo oLink.HotKey="F1"
+::echo oLink.IconLocation = "C:\Program Files\MyApp\MyProgram.EXE, 2"
 echo oLink.WindowStyle = "1"
 echo oLink.WorkingDirectory = "C:\Program Files\MyApp"
 echo oLink.Save
 ) > CreateShortcut.vbs
-::type CreateShortcut.vbs
-::powershell -Command "Start-Process cscript %cd%\CreateShortcut.vbs -Verb RunAs "
+type CreateShortcut.vbs
+powershell -Command "Start-Process cscript %cd%\CreateShortcut.vbs -Verb RunAs "
 ::explorer "C:\ProgramData\Microsoft\Windows\Start Menu\Programs"
-::del CreateShortcut.vbs
+pause
+del CreateShortcut.vbs
+del "%SpellRing%\%SpellName%"
 (TIMEOUT /T 10)&&(exit /b)
 :::::::::::::::::::::::::::::::::::::::::::::
